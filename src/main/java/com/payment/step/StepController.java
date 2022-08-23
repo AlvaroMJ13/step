7210ae5d3d021ce0c97815bd0e126b0c78a7704f
Alvaro Mateos <alvaro.mateos@serquo.com>
2022-08-23 13:17:31 +0200

    Initial version simple example step

---
+++ src/main/java/com/payment/step/StepController.java
@@ -0,0 +1,19 @@
+package com.payment.step;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/step")
+public class StepController {
+	
+	@GetMapping("/status")
+    public ResponseEntity<String> get() {
+		return ResponseEntity.ok("Step1 working");
+		
+	}
+
+}
