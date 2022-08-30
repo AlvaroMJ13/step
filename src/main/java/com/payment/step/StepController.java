f707f95e629b1245e37720633fc7a8dfcb4d1134
Alvaro Mateos <alvaro.mateos@serquo.com>
2022-08-30 12:59:30 +0200

    ADD request params

--- src/main/java/com/payment/step/StepController.java
+++ src/main/java/com/payment/step/StepController.java
@@ -3,15 +3,21 @@ package com.payment.step;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/step")
 public class StepController {
 	
 	@GetMapping("/status")
-    public ResponseEntity<String> get() {
+    public ResponseEntity<String> get(@RequestParam String gtsMessageId, @RequestParam String idExecution) {
+		
+		log.info("Gateway request: gtsMessageId {} - idExecution {}", gtsMessageId, idExecution);
+		
 		return ResponseEntity.ok("Step1 working");
 		
 	}
